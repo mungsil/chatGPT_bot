16a7bb56fae77fe5c350295eb7e838da0e4844bc
mungsil <thddms2009@naver.com>
2023-11-24 20:21:09 +0900

    Modify assignment

--- chatGPT.py
+++ chatGPT.py
@@ -5,24 +5,17 @@ from openai import OpenAI
 client = OpenAI(
     api_key=""
 )
-
-
-def ChatGPT():
-    completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+   messages=[
         {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
         {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
         ]
     )
-    return completion.choices[0].message.content
 
+body=completion.choices[0].message.content
 
-def job(answer):
-    token = "6928016911:AAEfmD-uwp2jYpTxdN35vlI18BvqfpJ_x3M"
-    bot = telegram.Bot(token = token)
-    public_chat_name = '@kopensw'
-    id_channel = asyncio.run(bot.sendMessage(chat_id=public_chat_name,text=answer)).chat_id
-    print(id_channel)
-
-job(ChatGPT())
+token = "6928016911:AAEfmD-uwp2jYpTxdN35vlI18BvqfpJ_x3M"
+bot = telegram.Bot(token = token)
+public_chat_name = '@kopensw'
+asyncio.run(bot.sendMessage(chat_id=public_chat_name,text=body))
